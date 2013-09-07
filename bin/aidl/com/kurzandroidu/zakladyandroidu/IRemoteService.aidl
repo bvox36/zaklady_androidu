53e2a8cc62fe4a64b6cd2434e07aa121f91b1c98
Vitezslav Kot <vitezslav.kot@gmail.com>
2013-09-07 14:17:04 +0200

    Konec lekce 4

--- bin/aidl/com/kurzandroidu/zakladyandroidu/IRemoteService.aidl
+++
@@ -1,6 +0,0 @@
-package com.kurzandroidu.zakladyandroidu;
-
-interface IRemoteService {
-
-	String[] getViewTypes();
-}
---
+++ src/com/kurzandroidu/zakladyandroidu/ActivityMultiRes.java
@@ -0,0 +1,12 @@
+package com.kurzandroidu.zakladyandroidu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityMultiRes extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
